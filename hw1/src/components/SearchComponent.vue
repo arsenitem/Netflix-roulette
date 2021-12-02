c9ca708c2a0b6e934c287e41239e475247f18b66
arsenitem <arsenitem@bk.ru>
2021-12-02 20:50:55 +0500

    added ts support + jest

---
+++ hw1/src/components/SearchComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <input/>
+      <input type="button"/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ hw1/src/components/SearchMovies.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="search-block">
+        <SearchComponent></SearchComponent>
+    </div>
+</template>
+
+<script>
+import SearchComponent from "./SearchComponent.vue"
+export default {
+    components: {
+        SearchComponent
+    }
+}
+</script>
+
+<style>
+    .search-block {
+        height: 300px;
+        background-image: url("https://miro.medium.com/max/4000/1*few-89t3Lc5M-WG4qQ8Uqg.jpeg");
+    }
+</style>
